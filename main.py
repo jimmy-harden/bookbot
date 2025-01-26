399d0fbf87be3c3c3c286d10f4b60941e245af34
jimmy-harden <mail@jimmy.au>
2025-01-26 14:28:05 +1000

    completed main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+def main():
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    words = count_book_words(text)
+    characters = character_count(text)
+    char_list = sorted_list(characters)
+    print_report(path, words, char_list)
+ 
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_book_words(text):
+    words = text.split()
+    return len(words)
+
+def character_count(text):
+    results = {}
+    for character in text:
+        if character.isalpha() == True:
+            character = character.lower()
+            if character in results:
+                results[character] += 1
+            else:
+                results[character] = 1
+    return results
+
+def sort_by(dict):
+    return dict["num"]
+
+def sorted_list(results):
+    char_list = []
+    for char, num in results.items():
+        char_dict = {}
+        char_dict["char"] = char
+        char_dict["num"] = num
+        char_list.append(char_dict)
+    char_list.sort(reverse=True, key=sort_by)
+    return char_list
+
+def print_report(path, words, char_list):
+    print(f"--- Begin report of {path} ---")
+    print(f"{words} were found in the document\n")
+    for entry in char_list:
+        char = entry["char"]
+        num = entry["num"]
+        print(f"The '{char}' character was found {num} times")    
+    print(f"--- End Report ---")
+
+main()
